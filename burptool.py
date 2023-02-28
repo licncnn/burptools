a425eafc81268c41d1593e289d36d45fabab0d28
licncnn <1018960866@qq.com>
2023-02-28 15:51:20 +0800

    basic function

--- burptool.py
+++ burptool.py
@@ -1,21 +1,13 @@
-import os 
+import os
 import sys
 import argparse
+import subprocess
+import time
+'''
+	自定义参数
 
-
-# parser = argparse.ArgumentParser(description='命令行中传入一个数字')
-# #type是要传入的参数的数据类型  help是该参数的提示信息
-# parser.add_argument('integers', type=str, help='传入的数字')
-
-# args = parser.parse_args()
-
-# #获得传入的参数，输出内容 Namespace(integers='5')
-# print(args)
-
-
-
-
-
+'''
+default_thread = 3
 
 def shellAccept():
     '''
@@ -29,8 +21,15 @@ def shellAccept():
     '''
     try:    # 异常处理
         parser = argparse.ArgumentParser(description="传入命令参数")
-        parser.add_argument("-u", "--URL", type=str, help="待测试的URL")
+        parser.add_argument("-u", "--url", type=str,required=True, help="待测试的URL")
         parser.add_argument("-t", "--threads", type=str, help="线程数")
+        parser.add_argument("-ssh", "--ssh", type=str, help="指定ssh爆破参数")
+        parser.add_argument("-ftp", "--ftp", type=str, help="指定ftp爆破参数")
+        parser.add_argument("-rdp", "--rdp", type=str, help="指定rdp爆破参数")
+        parser.add_argument("-mysql", "--mysql", type=str, help="指定mysql爆破参数")
+        parser.add_argument("-redis", "--redis", type=str, help="指定redis爆破参数")
+        parser.add_argument("-dir", "--dir", type=str, help="指定dirsearch参数")
+
         parser.add_argument("-v", "--version", type=str, help="工具版本号")
         args = parser.parse_args()  # 获取参数字典
         return args
@@ -38,9 +37,46 @@ def shellAccept():
         print(e)
 
 
-
-
-
 if __name__ == '__main__': 
-    shellAccept()
+    args = shellAccept()
+    thread = default_thread if args.threads==None else args.threads
+    if args.version !=None:
+    	print("version_1.0")
+    print("""
+            ||====================================
+            ||	url：  """+args.url+"""  
+            ||	线程数：   """+thread+ """   
+            ||====================================
+        """)
+    if args.ssh !=None:
+    	print("==================================================================检测到ssh参数 开始爆破ssh服务====================================================")
+    	print("")
+    	res = ""
+    	if args.ssh.isdigit():
+    		print("接收到ssh端口号"+args.ssh)
+    		cmd = "hydra "+args.url+" ssh -L ./dict/user.txt -P ./dict/pass.txt -s "+args.ssh+" -t "+thread+" -V -f |tee ssh.log"
+    	else:
+    		print("使用ssh 默认端口号")
+    		cmd = "hydra "+args.url+" ssh -L ./dict/user.txt -P ./dict/pass.txt -t "+thread+" -V -f |tee ssh.log"
+    	print("正在执行命令 :" + cmd)
+    	res=os.system(cmd)
+    	print("==================================================================ssh 扫描命令执行结束==================================================")
+    	print("\n")
+    	time.sleep(8)	
+    
+    if args.ftp !=None:
+    	print("==================================================================检测到ftp参数 开始爆破ftp服务====================================================")
+    	print("")
+    	res = ""
+    	if args.ftp.isdigit():
+    		print("接收到ftp端口号"+args.ftp)
+    		cmd = "hydra "+args.url+" ftp -L ./dict/user.txt -P ./dict/pass.txt -s "+args.ftp+" -t "+thread+" -V -f |tee ftp.log"
+    	else:
+    		print("使用ssh 默认端口号")
+    		cmd = "hydra "+args.url+" ftp -L ./dict/user.txt -P ./dict/pass.txt -t "+thread+" -V -f |tee ftp.log"
+    	print("正在执行命令 :" + cmd)
+    	res=os.system(cmd)
+    	print("==================================================================ftp 扫描命令执行结束==================================================")
+    	time.sleep(8)	
+    
 
